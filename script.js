18fee22ce5c47484ef3fbd313f4dc00b81ec0fda
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-02-22 13:40:14 +0600

    script

---
+++ script.js
@@ -0,0 +1,38 @@
+let button = document.querySelector('button');
+
+let inputName = document.querySelector('.name')
+let inputMassage = document.querySelector('.text')
+let inputColor = document.querySelector('.color')
+
+
+
+button.addEventListener('mousedown',  function(){
+  //  console.log(inputName.value)
+  //  console.log(inputMassage.value)
+  //  console.log(inputColor.value)
+
+
+let div = document.createElement('div');
+let spanName = document.createElement('span');
+let spanMessage = document.createElement('span');
+
+document.body.append(div);
+div.append(spanName, spanMessage);
+spanName.innerText = inputName.value + ": ";
+spanMessage.innerText = inputMassage.value;
+spanName.style.color = inputColor.value;
+
+
+
+
+fetch(' https://live-chat-40620-default-rtdb.firebaseio.com/', {
+  method:"POST" ,
+  body: JSON.stringify({
+    name: inputName.value,
+            message: inputMessage.value,
+            color: inputColor.value,
+
+  })
+})
+
+});
